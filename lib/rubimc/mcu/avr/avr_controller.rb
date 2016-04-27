fe15655a6ac1e3336ac1186048e93ff1cb4dcd71
Evgeny Danilov <jmelkor@rambler.ru>
2016-04-27 23:03:56 +0300

    create rubimc gem

---
+++ lib/rubimc/mcu/avr/avr_controller.rb
@@ -0,0 +1,14 @@
+class AVRController < Controllers
+	def self.print_layout(position)
+		if position == :before_main
+			print_cc_layout(:before_main) do 
+				micro_layout # prints includes for current microcontroller
+			end 
+		elsif position == :after_main
+			print_cc_layout(:after_main)
+		end
+	end
+end
+
+# ToDO: add all folder 'avr'
+require 'rubimc/mcu/avr/attiny13'
