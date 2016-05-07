6f7c31b61a25be840ccd5680cb1589fbde6753e5
Evgeny Danilov <jmelkor@rambler.ru>
2016-05-07 13:57:04 +0300

    add bool/float/double C-types

---
+++ examples/release/ex1_avr_tiny13.rb
@@ -0,0 +1,35 @@
+#####################################################################
+# Example: "AVR attiny13"                                           #
+# Author: Evgeny Danilov                                            #
+# Created at 2016-04-26                                             #
+#####################################################################
+
+require 'rubimc'
+
+class FirstController < AVR_attiny13
+    def initialize
+        @viar, var1 = integer :@viar, :var1
+        @viar .c_assign=  RubimCode::UserVariable.new(12, 'fixed')
+
+        ert = int :ert
+        gerth = bool :gerth
+        gerth .c_assign= RubimCode::UserVariable.new(false, 'fixed')
+
+        name_fl = double :name_fl
+        name_fl .c_assign=  RubimCode::UserVariable.new(12.4, 'fixed')
+        @viar .c_assign=  RubimCode::UserVariable.new(23, 'fixed') / name_fl
+
+        ANALOG_TO_DIGITAL.init(ref: "vcc", channel: ADC0)
+
+        ANALOG_TO_DIGITAL.interrupt(enabled: RubimCode::UserVariable.new(true, 'fixed')) do |volts|
+            output :led, port: :B, pin:  RubimCode::UserVariable.new(3, 'fixed')
+            led.off if RubimCode.rubim_ifmod volts <  RubimCode::UserVariable.new(30, 'fixed'); RubimCode.rubim_end;
+            led.on if RubimCode.rubim_ifmod volts >=  RubimCode::UserVariable.new(220, 'fixed'); RubimCode.rubim_end;
+        end
+    end
+
+    def main_loop # # infinit loop, it stop only when IC is reset
+        qwe = integer :qwe
+        qwe .c_assign= @viar
+    end
+end
